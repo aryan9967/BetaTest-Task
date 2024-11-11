7bb2747015214bf7c16141844f39bd0481702c05
Aryan <aryanmauryaofficial2601@gmail.com>
2024-11-11 20:17:59 +0530

    d-3

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -6,7 +6,7 @@ import Login from "./pages/Login";
 function App() {
   return (
     <Routes >
-      <Route path="/" element={<Login />} ></Route>
+      <Route path="/" element={<Dashboard />} ></Route>
       <Route path="/dashboard" element={<Dashboard />}></Route>
     </Routes>
     
